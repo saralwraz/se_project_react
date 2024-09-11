22a8c9e248a20bd3a782724ef0b1644a1eaa65ab
Sara Raz <saralwraz@gmail.com>
2024-09-11 21:55:15 +0300

    Added file structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "../src/components/App";
+import App from "./main.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
