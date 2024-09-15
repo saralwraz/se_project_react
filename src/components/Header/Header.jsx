b6491b3eefb8f5df8bb3d67f5f3ea6237ca8eefa
Sara Raz <saralwraz@gmail.com>
2024-09-15 15:04:35 +0300

    Header spacing and sizing.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,20 @@
 import "./Header.css";
+import logo from "../../assets/wtwr-logo.svg";
+import avatar from "../../assets/wtwr-avatar.svg";
 
 function Header() {
   return (
     <header className="header">
-      <img className="header__logo" />
+      <img className="header__logo" src={logo} />
       <p className="header__date_time">Date, Location</p>
       <button className="header__add_clothes">+ Add Clothes</button>
       <div className="header__user_info">
-        <p className="header__username">Name</p>{" "}
-        <img src="" alt="" className="header__user_avatar" />
+        <p className="header__username">Terrence Tegegne</p>{" "}
+        <img
+          src={avatar}
+          alt="Terrence Tegegne"
+          className="header__user_avatar"
+        />
       </div>
     </header>
   );
