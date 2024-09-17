bc5389cc4d410fdc9e9b92ae16761ca674c3fb83
Sara Raz <saralwraz@gmail.com>
2024-09-17 12:44:33 +0300

    ModalWithForm refactoring

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,7 +13,51 @@ function App() {
         <Header />
         <Main weatherData={weatherData} />
         <Footer />
-        <ModalWithForm />
+        <ModalWithForm buttonText="Add Garment" Title="New Garment">
+          <label htmlFor="name" className="modal__label">
+            Name{" "}
+            <input
+              type="text"
+              id="name"
+              className="modal__input"
+              placeholder="Name"
+            />
+          </label>
+
+          <label htmlFor="imageLink" className="modal__label">
+            Image{" "}
+            <input
+              type="url"
+              id="imageLink"
+              className="modal__input"
+              placeholder="Image URL"
+            />
+          </label>
+
+          <fieldset className="modal__radio_buttons">
+            <legend className="modal__legend">Select the weather type:</legend>
+            <label
+              htmlFor="hot"
+              className="modal__label modal__label_type_radio"
+            >
+              <input type="radio" id="hot" className="modal__radio_input" /> Hot
+            </label>
+            <label
+              htmlFor="warm"
+              className="modal__label modal__label_type_radio"
+            >
+              <input type="radio" id="warm" className="modal__radio_input" />{" "}
+              Warm
+            </label>
+            <label
+              htmlFor="cold"
+              className="modal__label modal__label_type_radio"
+            >
+              <input type="radio" id="cold" className="modal__radio_input" />{" "}
+              Cold
+            </label>
+          </fieldset>
+        </ModalWithForm>
       </div>
     </div>
   );
