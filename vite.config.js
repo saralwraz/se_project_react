b683280471bdc20a921ebbfea20a2a271b2e2907
Sara Raz <saralwraz@gmail.com>
2024-09-18 13:09:11 +0300

    GH pages integration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "se_project_reach-main",
   plugins: [react()],
-  // add the server object
   server: {
     port: 3000,
   },
