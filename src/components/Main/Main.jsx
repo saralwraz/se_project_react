3ae7f5b727b9a979105cb613096dc9061afb8de5
Sara Raz <saralwraz@gmail.com>
2024-09-20 00:14:34 +0300

    Tweak import statements

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import "./Main.css";
+import "./main.css";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import { defaultClothingItems } from "../../utils/constants";
 import ItemCard from "../ItemCard/ItemCard";
