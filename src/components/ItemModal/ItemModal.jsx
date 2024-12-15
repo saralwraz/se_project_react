19333de060d92fba4a4ef9b87b2e4bd2fa53b714
Sara Raz <saralwraz@gmail.com>
2024-12-15 22:07:04 +0200

    User context for item modal

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,6 +1,11 @@
+import { useContext } from "react";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 import "./ItemModal.css";
 
 function ItemModal({ activeModal, closeActiveModal, card, handleDelete }) {
+  const currentUser = useContext(CurrentUserContext);
+  const { name, imageUrl, weather } = card || {};
+  const isOwner = currentUser && currentUser?._id === card?.owner;
   return (
     <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
       <div className="modal__form modal__content_image">
@@ -13,9 +18,14 @@ function ItemModal({ activeModal, closeActiveModal, card, handleDelete }) {
         <div className="modal__footer">
           <h2 className="modal__text">{card.name}</h2>
           <p className="modal__weather">Weather: {card.weather}</p>
-          <button className="modal__delete" onClick={handleDelete}>
-            Delete item
-          </button>
+          {isOwner && (
+            <button className="modal__delete" onClick={handleDelete}>
+              Delete item
+            </button>
+          )}
+          {!isOwner && (
+            <p className="modal__error">You Cannot Delete This Item</p>
+          )}
         </div>
       </div>
     </div>
